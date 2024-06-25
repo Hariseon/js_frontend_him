596e144d0cb438e23abe0f3dc4f30f5b16fff507
Hariseon <danyl890@mail.ru>
2024-06-25 23:36:30 +0300

    Apply custom UI template

--- src/ToDoTaskAdd.js
+++ src/ToDoTaskAdd.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate,NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { todoAdd } from './actions';
 
 class ToDoTaskAddInner extends React.Component {
 
@@ -8,15 +10,15 @@ class ToDoTaskAddInner extends React.Component {
 		
 		this.state = {
 			name: '',
-			decription: ''
+			description: ''
 		}
 		
 		this.onNameChange = this.onNameChange.bind(this);
 		this.onDescriptionChange = this.onDescriptionChange.bind(this);
 		this.onAddFormSubmit = this.onAddFormSubmit.bind(this);
 		
-		}
-
+	}
+	
 	onNameChange(e) {
 		e.preventDefault();
 		
@@ -32,7 +34,7 @@ class ToDoTaskAddInner extends React.Component {
 			description: e.target.value
 		});
 	}
-
+	
 	onAddFormSubmit(e) {
 		e.preventDefault()
 		
@@ -46,18 +48,34 @@ class ToDoTaskAddInner extends React.Component {
 		}).then((res) => {
 			return res.json();
 		}).then((data) => {
-			this.props.onTaskAdd(data);
-			this.props.history('/')
+			this.props.dispatch(todoAdd(data._id, data.name, data.description));
+			this.props.history('/');
 		})
 	}
 
 	render() {
 		return (
-			<form onSubmit={this.onAddFormSubmit}>
-			<input type="text" value={this.state.name} onChange={this.onNameChange} placeholder='Name'/>
-			<input type="text" value={this.state.description} onChange={this.onDescriptionChange} placeholder='Description'/>
-			<input type="submit" value="Add" />
-			</form>
+		<div className="card-hover-shadow-2x mb-3 card">
+			<div className="card-header-tab card-header">
+				<div className="card-header-title font-size-lg text-capitalize font-weight-normal">
+					<i className="fa fa-tasks"></i>&nbsp;Add Task
+				</div>
+			</div>
+			<div className="form-group">
+				<form onSubmit={this.onAddFormSubmit}>
+					<div className="widget-content">
+						<div className="widget-content-wrapper">
+							<input type="text" value={this.state.name} onChange={this.onNameChange} placeholder='Name' className="form-control"/>
+							<input type="text" value={this.state.description} onChange={this.onDescriptionChange} placeholder='Description' className="form-control"/>
+							<input type="submit" value="Add" className="btn btn-primary"/>
+						</div>
+					</div>
+				</form>
+			</div>
+			<div className="d-block text-right card-footer">
+				<NavLink to='/' className="btn btn-primary">Back to list</NavLink>
+			</div>
+		</div>
 		)
 	}
 }
@@ -68,4 +86,4 @@ const ToDoTaskAdd = (props) => {
 	)
 }
 
-export default ToDoTaskAdd;
+export default connect()(ToDoTaskAdd);
